b5e7343064d10ab3d328c638b6ab3a6c5dd6f96d
Joshua Noronha <joshuanoronha@pop-os.localdomain>
2020-04-10 16:16:46 +0530

    Added Fetch Pull Request from Github API

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
 import React from "react";
 import { connect } from "react-redux";
-
 import { createTestAdd, createTestClear } from "./actions";
+import keys from "./keys.json";
 import "./App.css";
 
 const App = (props) => {
   const buttonPress = (event) => {
-    props.createTestAdd("Testing this");
-    event.stopPropagation();
-  }
+    fetch("https://api.github.com/repos/joshua-noronha13/portfolio/pulls", {
+      headers: new Headers({
+        Authorization: keys.personalAccessToken,
+      }),
+    })
+      .then((response) => response.json())
+      .then((pullRequests) => {
+        console.log(pullRequests);
+        props.createTestAdd(pullRequests[0].base.label);
+        event.stopPropagation();
+      });
+  };
 
   const clear = (e) => {
     props.createTestClear();
     e.stopPropagation();
-  }
+  };
 
   return (
     <div className="App">
-      <div id="add" className="button" onClick={buttonPress}>Add</div>
-      <div id="clear" className="button" onClick={clear}>Clear</div>
+      <div id="add" className="button" onClick={buttonPress}>
+        Add
+      </div>
+      <div id="clear" className="button" onClick={clear}>
+        Clear
+      </div>
       <br />
       <ol>
-        {props.list.map((x,i) => <li key={i}>{x}</li>)}
+        {props.list.map((x, i) => (
+          <li key={i}>{x}</li>
+        ))}
       </ol>
     </div>
   );
-}
-
-const mapStateToProps = state => {
-  return { list: [...state.list]  };
 };
 
-export default connect(mapStateToProps, { createTestAdd, createTestClear })(App);
+const mapStateToProps = (state) => {
+  return { list: [...state.list] };
+};
+
+export default connect(mapStateToProps, { createTestAdd, createTestClear })(
+  App
+);
 // export default App;
--- src/contentScript/app.js
+++ src/contentScript/app.js
@@ -2,33 +2,41 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { createTestAdd, createTestClear } from "../actions";
-import "./App.css";
+import "./app.css";
 
 const App = (props) => {
   const buttonPress = (event) => {
     props.createTestAdd("Testing this");
     event.stopPropagation();
-  }
+  };
 
   const clear = (e) => {
     props.createTestClear();
     e.stopPropagation();
-  }
+  };
 
   return (
     <div className="App">
-      <div id="add" className="button" onClick={buttonPress}>Add</div>
-      <div id="clear" className="button" onClick={clear}>Clear</div>
+      <div id="add" className="button" onClick={buttonPress}>
+        Add
+      </div>
+      <div id="clear" className="button" onClick={clear}>
+        Clear
+      </div>
       <br />
       <ol>
-        {props.list.map((x,i) => <div key={i}>{x}</div>)}
+        {props.list.map((x, i) => (
+          <div key={i}>{x}</div>
+        ))}
       </ol>
     </div>
   );
-}
-
-const mapStateToProps = state => {
-  return { list: [...state.list]  };
 };
 
-export default connect(mapStateToProps, { createTestAdd, createTestClear })(App);
+const mapStateToProps = (state) => {
+  return { list: [...state.list] };
+};
+
+export default connect(mapStateToProps, { createTestAdd, createTestClear })(
+  App
+);
